e82b72ed081f680401910606fa494ff236a0c09e
Hurshal Patel <hpatel516@gmail.com>
2015-10-31 18:42:03 -0700

    :star: more stars pls

--- static/js/props.js
+++ static/js/props.js
@@ -14,15 +14,18 @@ export var Starfield = function(num, radius, filter) {
         var theta = Math.acos(costheta);
         // generate some more stars near the center for exaggerated parallax effect
         var R, r;
-        if (u < 0.4) {
+        if (u < 0.2) {
             R = radius * Math.pow(u, 0.5);
             r = 0.5 + 2 * R / radius * Math.random();
-        } else if (u < 0.7) {
+        } else if (u < 0.5) {
             R = radius * 10 * u / 0.4;
             r = 10 + 2 * R / radius * Math.random();
-        } else {
+        } else if (u < 0.95) {
             R = radius * 20 * u / 0.7;
             r = 20 + 2 * R / radius * Math.random();
+        } else {
+            R = radius * 20 * u / 0.95;
+            r = 30 + 2 * R / radius * Math.random();
         }
 
         var x = R * Math.sin(theta) * Math.cos(phi);
